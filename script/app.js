021e6180ab190bf562bdbcb3ef64710c31f186ac
Anvar7777777 <toshpolatovanvar777@gmail.com>
2022-10-29 08:46:55 +0500

    Dictionary app

---
+++ script/app.js
@@ -0,0 +1,44 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+const form = document.querySelector("#form");
+const container = document.querySelector(".container")
+btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="container">
+            <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>
+                </div>`;
+            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+            form.appendChild(container);
+          
+        })
+        
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+});
+function playSound() {
+    sound.play();
+}
